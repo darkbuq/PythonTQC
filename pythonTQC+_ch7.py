0a706fdfcf2671f023e3219208c2faac7ceada23
chen yan rong <darkbuq@gmail.com>
2020-04-19 17:30:25 +0800

    707 ok

--- pythonTQC+_ch7.py
+++ pythonTQC+_ch7.py
@@ -331,27 +331,62 @@ end
 ['Chemistry', 'Chinese', 'Physical']
 ['Chemistry', 'Chinese', 'English', 'Geography', 'History', 'Physical']
 """
-a=set()
+x=set()
+y=set()
 print("Enter group X's subjects:")
 while True:
-    x=input()
-    if x=='end':
-        break
-    a.add(x)
-
-b=set()
+  enter=input()
+  if enter=="end":
+    break
+  x.add(enter)
+ 
 print("Enter group Y's subjects:")
 while True:
-    x=input()
-    if x=='end':
-        break
-    b.add(x)
+  enter=input()
+  if enter=="end":
+    break
+  y.add(enter)
+ 
+print(sorted(x|y))
+print(sorted(x&y))
+print(sorted(y-x))
+print(sorted(x^y))
+#%%
+"""
+TQC+ 程式語言Python 708 詞典合併
+請撰寫一程式，自行輸入兩個詞典
+（以輸入鍵值"end"作為輸入結束點，詞典中將不包含鍵值"end"），
+將此兩詞典合併，並根據key值字母由小到大排序輸出，
+如有重複key值，後輸入的key值將覆蓋前一key值。
 
-print(sorted(a|b))
-print(sorted(a&b))
-print(sorted(a-b))
-print(sorted(a^b))
+輸入說明
+輸入兩個詞典，直至end結束輸入
+輸出說明
+合併兩詞典，並根據key值字母由小到大排序輸出，
+如有重複key值，後輸入的key值將覆蓋前一key值
 
+輸入輸出範例
+輸入與輸出會交雜如下，輸出的部份以粗體字表示
+Create dict1:
+Key: a
+Value: apple
+Key: b
+Value: banana
+Key: d
+Value: durian
+Key: end
+Create dict2:
+Key: c
+Value: cat
+Key: e
+Value: elephant
+Key: end
+a: apple
+b: banana
+c: cat
+d: durian
+e: elephant
+"""
 
 
 
